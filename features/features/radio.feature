c9580806b1865e21ff4a0f8b3b2ce9aac0b5b184
Nat Ritmeyer <nat@natontesting.com>
2010-05-18 18:46:50 +0100

    test for selecting a radio button

--- features/features/radio.feature
+++ features/features/radio.feature
@@ -4,8 +4,14 @@ Feature: radio interaction
     In order to do my job
     I want to be able to interact with radio buttons
 
-    @wip
     Scenario: get state of unselected radio button
     Given I start the test app
     And I select the radio tab
-    Then the first radio button is unchecked
+    Then the third radio button is unchecked
+
+    @wip
+    Scenario: select radio button
+    Given I start the test app
+    And I select the radio tab
+    When I select the first radio button
+    Then the first radio button is selected
--- features/step_definitions/radio_steps.rb
+++ features/step_definitions/radio_steps.rb
@@ -2,6 +2,14 @@ Given /^I select the radio tab$/ do
   @main_window.get(:id => "main_tabs").select("checks/radio") if @main_window.get(:id => "main_tabs").selected.name != "checks/radio"
 end
 
-Then /^the first radio button is unchecked$/ do
-  @main_window.get(:id => "radioButton1").should_not be_selected
+Then /^the third radio button is unchecked$/ do
+  @main_window.get(:id => "radioButton3").should_not be_selected
+end
+
+When /^I select the first radio button$/ do
+  @main_window.get(:id => "radioButton1").select
+end
+
+Then /^the first radio button is selected$/ do
+  @main_window.get(:id => "radioButton1").should be_selected
 end

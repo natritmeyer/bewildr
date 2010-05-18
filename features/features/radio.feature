aa0edd10f3820a01f9fe9699546b240009c6e214
Nat Ritmeyer <nat@natontesting.com>
2010-05-18 18:57:31 +0100

    finished radio button tests

--- features/features/radio.feature
+++ features/features/radio.feature
@@ -7,11 +7,24 @@ Feature: radio interaction
     Scenario: get state of unselected radio button
     Given I start the test app
     And I select the radio tab
-    Then the third radio button is unchecked
+    Then the third radio button is unselected
 
-    @wip
     Scenario: select radio button
     Given I start the test app
     And I select the radio tab
     When I select the first radio button
     Then the first radio button is selected
+
+    Scenario: select radio button using click
+    Given I start the test app
+    And I select the radio tab
+    When I select the second radio button using click
+    Then the second radio button is selected
+    And the third radio button is unselected
+
+    @wip
+    Scenario: cycle through radio buttons
+    Given I start the test app
+    And I select the radio tab
+    When I loop through each radio button, clicking each one
+    Then the third radio button is selected
--- features/step_definitions/radio_steps.rb
+++ features/step_definitions/radio_steps.rb
@@ -2,7 +2,7 @@ Given /^I select the radio tab$/ do
   @main_window.get(:id => "main_tabs").select("checks/radio") if @main_window.get(:id => "main_tabs").selected.name != "checks/radio"
 end
 
-Then /^the third radio button is unchecked$/ do
+Then /^the third radio button is unselected$/ do
   @main_window.get(:id => "radioButton3").should_not be_selected
 end
 
@@ -12,4 +12,24 @@ end
 
 Then /^the first radio button is selected$/ do
   @main_window.get(:id => "radioButton1").should be_selected
+end
+
+When /^I select the second radio button using click$/ do
+  @main_window.get(:id => "radioButton2").click
+end
+
+Then /^the second radio button is selected$/ do
+  @main_window.get(:id => "radioButton2").should be_selected
+end
+
+When /^I loop through each radio button, clicking each one$/ do
+  @main_window.get(:id => "tab_checks_radio").get(:type => :radio_button).each do |radio|
+    radio.should_not be_selected
+    radio.select
+    radio.should be_selected
+  end
+end
+
+Then /^the third radio button is selected$/ do
+  @main_window.get(:id => "radioButton3").should be_selected
 end

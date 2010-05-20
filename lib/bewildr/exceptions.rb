b3c7cea453ff468f4d60d7e578362d6ac4bf5e52
Nat Ritmeyer <nat@natontesting.com>
2010-05-20 20:06:45 +0100

    adding new exception type

--- lib/bewildr/exceptions.rb
+++ lib/bewildr/exceptions.rb
@@ -4,5 +4,6 @@ class ElementNotEnabled < StandardError; end
 class ElementDoesntExist < StandardError; end
 class PasswordFieldReadAttempt < StandardError; end
 class NoSuchItemInComboBox < StandardError; end
+class NoSuchItemInListBox < StandardError; end
 
 class BewildrInternalError < StandardError; end

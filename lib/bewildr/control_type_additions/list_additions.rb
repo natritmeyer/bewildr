bf7fe65490332bc00627c10a6ff5737df2125048
Nat Ritmeyer <nat@natontesting.com>
2010-05-20 20:13:39 +0100

    adding functionality to list additions, methods based on combobox. tests to follow.

--- lib/bewildr/control_type_additions/list_additions.rb
+++ lib/bewildr/control_type_additions/list_additions.rb
@@ -11,6 +11,45 @@ module Bewildr
             selectable_elements = get(:type => :list_item, :scope => :children)
             selectable_elements.find {|selectable_element| selectable_element.name == input}.select
           end
+
+          def items
+            my_list_items = list_items
+            return nil if my_list_items.nil?
+            my_list_items.collect {|item| item.name}
+          end
+
+          def list_items
+            bewildr_list_items = get(:type => :list_item, :scope => :children)
+            bewildr_list_items.nil? ? nil : bewildr_list_items
+          end
+
+          def count
+            my_items = items
+            my_items.nil? ? 0 : my_items.size
+          end
+
+          def select(input)
+            case input
+            when String then select_by_name(input)
+            when Integer then select_by_index(input)
+            else raise ArgumentError, "Select by name or by index"
+            end
+          end
+
+          def select_by_name(input)
+            my_item = list_items.find {|item| item.name == input}
+            raise NoSuchItemInListBox if my_item.nil?
+            my_item.select
+          end
+
+          def select_by_index(input)
+            raise "Index must be 0 or greater" if input < 0
+            my_item = list_items[input].select
+          end
+
+          def selected
+            get_selection.first.name
+          end
         end
       end
     end

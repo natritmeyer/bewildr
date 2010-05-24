7986eac9b1c773b9f7f5c5fbb16ad0bf24a90692
Nat Ritmeyer <nat@natontesting.com>
2010-05-25 00:06:51 +0100

    implemented rudimentary select_all and get_text methods on text pattern

--- lib/bewildr/control_patterns/text_pattern.rb
+++ lib/bewildr/control_patterns/text_pattern.rb
@@ -3,7 +3,13 @@
 module Bewildr
   module ControlPatterns
     module TextPattern
-      
+      def select_all
+        @automation_element.get_current_pattern(System::Windows::Automation::TextPattern.pattern).document_range.select
+      end
+
+      def get_text
+        @automation_element.get_current_pattern(System::Windows::Automation::TextPattern.pattern).document_range.get_text(-1)
+      end
     end
   end
 end

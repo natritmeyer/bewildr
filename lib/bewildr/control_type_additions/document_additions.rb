652ec8767d4fd6737fac840b0c6057766157eece
Nat Ritmeyer <nat@natontesting.com>
2010-05-25 00:07:22 +0100

    implemented text method, now need to do text= method

--- lib/bewildr/control_type_additions/document_additions.rb
+++ lib/bewildr/control_type_additions/document_additions.rb
@@ -7,7 +7,13 @@ module Bewildr
         base.extend Bewildr::ControlPatterns::TextPattern
 
         base.instance_eval do
+          def text
+            get_text
+          end
 
+          def text=(input)
+            
+          end
         end
       end
     end

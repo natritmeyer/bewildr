f335026bfc2c13b6f772d31828b7c671bd0e485e
Nat Ritmeyer <nat@natontesting.com>
2010-06-01 22:11:03 +0100

    fixed tree code in test app

--- support/BewildrTestApp/BewildrTestApp/Window1.xaml.cs
+++ support/BewildrTestApp/BewildrTestApp/Window1.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Windows;
+using System.Data;
 using System.Windows.Controls;
 
 namespace BewildrTestApp
@@ -39,6 +41,34 @@ namespace BewildrTestApp
 
             //set three state checkbox to be intermediate
             three_state_checkbox.IsChecked = null;
+
+            //create the table
+            DataSet data_set = new DataSet();
+            DataTable data_table = new DataTable("testdata");
+
+            //create the table columns
+            DataColumn col1 = new DataColumn("col 1", typeof(string));
+            DataColumn col2 = new DataColumn("col 2", typeof(string));
+            DataColumn col3 = new DataColumn("col 3", typeof(string));
+
+            //add the columns to the datatable
+            data_table.Columns.Add(col1);
+            data_table.Columns.Add(col2);
+            data_table.Columns.Add(col3);
+
+            //add some rows
+            data_table.Rows.Add("1", "Alice", "good");
+            data_table.Rows.Add("2", "Bob", "good");
+            data_table.Rows.Add("3", "Eve", "bad");
+
+            //add data table to data set
+            data_set.Tables.Add(data_table);
+
+            //dataGrid1.SetBinding
+
+            //dataGrid1.SetBinding(data_set, "testdata");
+
+            //dataGrid1.ItemsSource = data_set.Tables[0].AsEnumerable();
         }
 
         private void enabled_button_Click(object sender, RoutedEventArgs e)
@@ -76,34 +106,14 @@ namespace BewildrTestApp
             sub_sub_level_menu_result.Content = "selected!";
         }
 
-        private void tree_l1_double_clicked(object sender, RoutedEventArgs e)
+        private void tree_double_clicked(object sender, RoutedEventArgs e)
         {
-            tree_results.Content = "level 1 double clicked";
+            tree_results.Content = ((TreeViewItem)treeView1.SelectedItem).Header + " double clicked";
         }
 
-        private void tree_l2_double_clicked(object sender, RoutedEventArgs e)
+        private void tree_selected(object sender, RoutedEventArgs e)
         {
-            tree_results.Content = "level 2 double clicked";
-        }
-
-        private void tree_l3_double_clicked(object sender, RoutedEventArgs e)
-        {
-            tree_results.Content = "level 3 double clicked";
-        }
-
-        private void tree_l1_selected(object sender, RoutedEventArgs e)
-        {
-            tree_results.Content = "level 1 selected";
-        }
-
-        private void tree_l2_selected(object sender, RoutedEventArgs e)
-        {
-            tree_results.Content = "level 2 selected";
-        }
-
-        private void tree_l3_selected(object sender, RoutedEventArgs e)
-        {
-            tree_results.Content = "level 3 selected";
+            tree_results.Content = ((TreeViewItem)treeView1.SelectedItem).Header + " selected";
         }
     }
 }

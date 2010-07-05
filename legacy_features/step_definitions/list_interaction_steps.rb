94725f2fe05e2ccb1fe20e90b5f1a93b9e1e6a4d
Nat Ritmeyer <nat@natontesting.com>
2010-07-05 23:47:10 +0100

    removing legacy tests - fully ported over to new custom test app

--- legacy_features/step_definitions/list_interaction_steps.rb
+++
@@ -1,7 +0,0 @@
-When /^I select One from the right hand list$/ do
-  @main_window.get(:id => "listBoxWithDynamicItems").select("One")
-end
-
-Then /^the selected item in the right hand list is One$/ do
-  @main_window.get(:id => "listBoxWithDynamicItems").selected.name.should match("One")
-end

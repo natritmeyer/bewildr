89cae792c22347a880f4ea7727ca0541bf876001
Nat Ritmeyer <nat@natontesting.com>
2010-08-15 16:44:30 +0100

    newline functionality implemented, all tests passing

--- lib/bewildr/control_type_additions/document_additions.rb
+++ lib/bewildr/control_type_additions/document_additions.rb
@@ -7,15 +7,22 @@ module Bewildr
         base.extend Bewildr::ControlPatterns::TextPattern
 
         base.instance_eval do
+
+          #TODO: make this deal with other special chars
+          def clean_string(input)
+            input.gsub(/\n/, "{ENTER}")
+          end
+
           def text
             get_text
           end
 
           def text=(input)
+            cleaned_string = clean_string(input)
             focus
             select_all
             System::Windows::Forms::SendKeys.send_wait("{DEL}")
-            System::Windows::Forms::SendKeys.send_wait(input)
+            System::Windows::Forms::SendKeys.send_wait(cleaned_string)
           end
         end
       end

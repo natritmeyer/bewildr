1115917ce0d91ff61c779d21d056aa13a8a42ec3
Nat Ritmeyer <nat@natontesting.com>
2011-02-08 22:05:00 +0000

    adding rdoc to document additions, making clean_string private

--- lib/bewildr/control_type_additions/document_additions.rb
+++ lib/bewildr/control_type_additions/document_additions.rb
@@ -3,6 +3,22 @@
 module Bewildr
   module ControlTypeAdditions
     module DocumentAdditions
+      #Returns the document text
+      def text
+        get_text
+      end
+
+      #Sets the document text to the supplied input
+      def text=(input)
+        cleaned_string = clean_string(input)
+        focus
+        select_all
+        System::Windows::Forms::SendKeys.send_wait("{DEL}")
+        System::Windows::Forms::SendKeys.send_wait(cleaned_string)
+      end
+
+      private
+
       def clean_string(input)
         #add special chars as necessary. Ref: http://msdn.microsoft.com/en-us/library/system.windows.forms.sendkeys.send.aspx
         replacements = {
@@ -11,18 +27,6 @@ module Bewildr
         cleaned_string = input
         replacements.keys.each {|key| cleaned_string.gsub!(key, replacements[key])}
         cleaned_string
-      end
-
-      def text
-        get_text
-      end
-
-      def text=(input)
-        cleaned_string = clean_string(input)
-        focus
-        select_all
-        System::Windows::Forms::SendKeys.send_wait("{DEL}")
-        System::Windows::Forms::SendKeys.send_wait(cleaned_string)
       end
     end
   end

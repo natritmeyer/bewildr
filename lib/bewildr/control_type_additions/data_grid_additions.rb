2aa5b9b1b57b699bf1af7c29c3afd2e0198bc5de
Nat Ritmeyer <nat@natontesting.com>
2011-02-08 21:59:27 +0000

    adding rdoc to data grid additions

--- lib/bewildr/control_type_additions/data_grid_additions.rb
+++ lib/bewildr/control_type_additions/data_grid_additions.rb
@@ -3,6 +3,7 @@
 module Bewildr
   module ControlTypeAdditions
     module DataGridAdditions
+      #Returns the cell at the supplied row and column
       def cell(row, column)
         get_item(row, column)
       end

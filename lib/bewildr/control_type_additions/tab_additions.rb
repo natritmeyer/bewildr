ff4bd13dfc8ae580a4092ca6e38f0de25b6ce4a6
Nat Ritmeyer <nat@natontesting.com>
2011-02-08 22:18:44 +0000

    adding rdoc to tab additions

--- lib/bewildr/control_type_additions/tab_additions.rb
+++ lib/bewildr/control_type_additions/tab_additions.rb
@@ -3,20 +3,24 @@
 module Bewildr
   module ControlTypeAdditions
     module TabAdditions
+      #Selects the tab whose name matches the input
       def select(input)
         raise Bewildr::NoSuchTab unless tab_names.include?(input)
         selectable_elements = tabs
         selectable_elements.find {|selectable_element| selectable_element.name == input}.select
       end
 
+      #Returns an array containing the tab items belonging to the tab control
       def tabs
         get(:type => :tab_item, :scope => :children, :how_many => :all)
       end
 
+      #Returns a string array containing the names of all the tabs in the tab control
       def tab_names
         tabs.collect {|tab| tab.name}
       end
 
+      #Returns the selected tab
       def selected
         tabs.find {|tab| tab.selected?}
       end

795d84081960fc86e307813f56446b8ea0b948c6
Nat Ritmeyer <nat@natontesting.com>
2011-02-08 21:57:35 +0000

    adding rdoc to combobox additions

--- lib/bewildr/control_type_additions/combo_box_additions.rb
+++ lib/bewildr/control_type_additions/combo_box_additions.rb
@@ -3,12 +3,14 @@
 module Bewildr
   module ControlTypeAdditions
     module ComboBoxAdditions
+      #Returns a string array containing the element's item names
       def items
         my_list_items = list_items
         return nil if my_list_items.nil?
         my_list_items.collect {|item| item.name}
       end
 
+      #Returns an array containing the combobox items
       def list_items
         begin
           expand_combo
@@ -19,11 +21,13 @@ module Bewildr
         end
       end
 
+      #Returns the number of items in the combobox
       def count
         my_items = items
         my_items.nil? ? 0 : my_items.size
       end
 
+      #Selects a combobox item. Takes a string (and selects the first item whose name matches) or an integer and selects the respective element
       def select(input)
         case input
         when String then select_by_name(input)
@@ -32,6 +36,7 @@ module Bewildr
         end
       end
 
+      #Selects the first item whose name matches the input
       def select_by_name(input)
         begin
           expand_combo
@@ -43,6 +48,7 @@ module Bewildr
         end
       end
 
+      #Selects the item at the supplied position
       def select_by_index(input)
         raise "Index must be 0 or greater" if input < 0
         begin
@@ -53,6 +59,7 @@ module Bewildr
         end
       end
 
+      #Returns the selected combobox item
       def selected
         #TODO: find a way to not need to expand and collapse before getting the selected item
         expand_combo
@@ -61,6 +68,7 @@ module Bewildr
         get_selection.first.name
       end
 
+      #Expands the combobox
       def expand_combo
         expand
         Timeout.timeout(30) do
@@ -68,6 +76,7 @@ module Bewildr
         end
       end
 
+      #Collapses the combobox
       def collapse_combo
         collapse
         Timeout.timeout(30) do

2ca3c5843c0c0bf76353dcdc15b6ea27976f7e7d
Nat Ritmeyer <nat@natontesting.com>
2011-02-08 21:12:52 +0000

    adding rdoc to expand_collapse pattern

--- lib/bewildr/control_patterns/expand_collapse_pattern.rb
+++ lib/bewildr/control_patterns/expand_collapse_pattern.rb
@@ -3,14 +3,21 @@
 module Bewildr
   module ControlPatterns
     module ExpandCollapsePattern
+      #Expands the element - use to open combo boxes
       def expand
         @automation_element.get_current_pattern(System::Windows::Automation::ExpandCollapsePattern.pattern).expand unless expand_state == :expanded or expand_state == :leaf
       end
 
+      #Collapses the element - use to close combo boxes
       def collapse
         @automation_element.get_current_pattern(System::Windows::Automation::ExpandCollapsePattern.pattern).collapse unless expand_state == :collapsed or expand_state == :leaf
       end
 
+      #Returns the expand-state of the element. The available states are:
+      #  :collapsed
+      #  :expanded
+      #  :partially_expanded
+      #  :leaf
       def expand_state
         case @automation_element.get_current_pattern(System::Windows::Automation::ExpandCollapsePattern.pattern).current.expand_collapse_state
         when System::Windows::Automation::ExpandCollapseState.collapsed           then return :collapsed

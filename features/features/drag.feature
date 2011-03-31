feb67cedd650c946c3ce585b815c984ca4a85c05
Nat Ritmeyer <nat@natontesting.com>
2011-03-31 17:23:34 +0100

    removing legacy drag and drop test

--- features/features/drag.feature
+++ features/features/drag.feature
@@ -4,12 +4,6 @@ Feature: drag and drop
     In order to do my job
     I want to be able to drag and drop ui elements
 
-    Scenario: be able to get a handle on the mouse
-    Given I start the test app
-    And I select the drag'n'drop tab
-    Then I can get hold of the bewildr mouse
-    And the mouse responds to the drag method
-
     Scenario: drag the label from one point to another
     Given I ensure that there are no instances of the test app running
     Given I start the test app
--- features/step_definitions/drag_steps.rb
+++ features/step_definitions/drag_steps.rb
@@ -2,14 +2,6 @@ Given /^I select the drag'n'drop tab$/ do
   @main_window.get(:id => "main_tabs").select("drag'n'drop") if @main_window.get(:id => "main_tabs").selected.name != "drag'n'drop"
 end
 
-Then /^I can get hold of the bewildr mouse$/ do
-  Bewildr::Mouse.should be_an_instance_of(Class)
-end
-
-Then /^the mouse responds to the drag method$/ do
-  Bewildr::Mouse.should respond_to(:drag)
-end
-
 Then /^I can drag the label from one point to another by referencing the element$/ do
   drag_label  = @main_window.get(:id => "drag_lbl")
   drag_target = @main_window.get(:id => "drag_target")
